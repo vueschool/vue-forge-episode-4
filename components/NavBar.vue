70daad92f73296da122764869fd372c1d972eb88
Daniel Kelly <me@danielkelly.io>
2023-07-17 18:34:41 -0500

    categories in navbar from supabase

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,51 +1,61 @@
 <script setup lang="ts">
-import { CategoryT, User } from '~/types'
-const {data: categoriesData} = useFetch('/api/categories')
-const categories = computed<CategoryT[]>(() => categoriesData.value?.data || [])
+import { User } from "~/types";
+const { fetchAll: fetchCategories, list: categories } = useCategories();
+await fetchCategories();
 
-
-const {data: userData} = useFetch('/api/auth/me')
-const user = computed<User>(() => userData.value?.data)
+const { data: userData } = await useFetch("/api/auth/me");
+const user = computed<User>(() => userData.value?.data);
 </script>
 
 <template>
-	<div class="bg-primary text-primary-content w-full fixed z-20">
-		<div class="navbar max-w-7xl mx-auto flex items-center justify-between">
-			<div class="flex-1">
-				<nuxt-link to="/" class="btn btn-ghost normal-case text-xl">DecentralSpark</nuxt-link>
-			</div>
-			<div class="flex-none">
-				<ul class="menu menu-horizontal px-1">
-					<li v-for="category in categories" :key="category.uuid">
-						<a>{{ category.name }}</a>
-					</li>
-				</ul>
-			</div>
-			<div class="flex-none gap-2">
-			<div class="form-control">
-				<nuxt-link :to="{ name: 'projects-create' }" class="btn btn-accent btn-sm">
-					Create Your Project
-				</nuxt-link>
-			</div>
-			<div class="dropdown dropdown-end">
-				<div tabindex="0" class="btn btn-ghost btn-circle avatar">
-					<div class="w-10 rounded-full">
-						<img :src="user.avatar"  alt=""/>
-					</div>
-				</div>
-				<ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-					<li>
-						<a class="justify-between">
-							Profile
-							<span class="badge">New</span>
-						</a>
-					</li>
-					<li><a>Settings</a></li>
-					<li><a>Logout</a></li>
-				</ul>
-			</div>
-		</div>
-		</div>
-	</div>
-
+  <div class="fixed z-20 w-full bg-primary text-primary-content">
+    <div class="flex items-center justify-between mx-auto navbar max-w-7xl">
+      <div class="flex-1">
+        <nuxt-link to="/" class="text-xl normal-case btn btn-ghost"
+          >DecentralSpark</nuxt-link
+        >
+      </div>
+      <div class="flex-none">
+        <ul class="px-1 menu menu-horizontal">
+          <li v-for="category in categories" :key="category.uuid">
+            <NuxtLink
+              :title="category.uuid"
+              :to="`/categories/${category.uuid}`"
+              >{{ category.name }}</NuxtLink
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="flex-none gap-2">
+        <div class="form-control">
+          <nuxt-link
+            :to="{ name: 'projects-create' }"
+            class="btn btn-accent btn-sm"
+          >
+            Create Your Project
+          </nuxt-link>
+        </div>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img :src="user.avatar" alt="" />
+            </div>
+          </div>
+          <ul
+            tabindex="0"
+            class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
